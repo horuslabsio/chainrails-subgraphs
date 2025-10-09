6fadc31b38fa53a57772b899c6813bc6dc6d8c71
Oche <ejembioche50@gmail.com>
2025-10-09 12:13:49 +0100

    fix: update intent status from PENDING to INITIATED in intent-factory mapping

--- chainrails/1.0.0/mappings/intent-factory.ts
+++ chainrails/1.0.0/mappings/intent-factory.ts
@@ -63,7 +63,7 @@ export function handleIntentCreated(event: IntentCreated): void {
         return;
     }
 
-    intent.status = "PENDING";
+    intent.status = "INITIATED";
     intent.creator = event.params.creator.toHexString();
 
     intent.save();
